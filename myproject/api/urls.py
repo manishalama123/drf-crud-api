9b758649e3d5b458f7742fc82d7fa77782416891
manishalama123 <manishalama136@gmail.com>
2025-02-17 07:35:47 +0545

    Added DRF CRUD (PUT & DELETE)

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.get_user, name = 'get_user'), 
-    path('post/', views.post_user, name = 'post_user'),
+    path('create/', views.post_user, name = 'post_user'),
+    path('<int:pk>/update/', views.update_user, name = 'update_user'),
+    path('<int:pk>/delete/', views.delete_user, name = 'delete_user'),
+
 ]
--- myproject/api/views.py
+++ myproject/api/views.py
@@ -20,5 +20,27 @@ def post_user(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    
+
+@api_view(['PUT'])
+def update_user(request, pk):
+    try:
+        person = Person.objects.get(pk=pk)
+    except Person.DoesNotExist:
+        return Response({'error': 'NOt found'}, status=status.HTTP_401_NOT_FOUND)
+    serializer = PersonSerializer(person, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def delete_user(request, pk):
+    try:
+        person = Person.objects.get(pk=pk)
+    except Person.DoesNotExist:
+        return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    person.delete()
+    return Response({'message': 'User deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
 
