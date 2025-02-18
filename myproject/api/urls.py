43926f5710de64a725ab6e6a5aa5ec38fbb88b74
manishalama123 <manishalama136@gmail.com>
2025-02-18 08:55:32 +0545

    Updated class based views and tested CRUD operations

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from . import views
+from .views import PersonListView, PersonDetailView
 
 urlpatterns = [
-    path('', views.get_user, name = 'get_user'), 
-    path('create/', views.post_user, name = 'post_user'),
-    path('<int:pk>/update/', views.update_user, name = 'update_user'),
-    path('<int:pk>/delete/', views.delete_user, name = 'delete_user'),
+    # path('', views.get_user, name = 'get_user'), 
+    # path('create/', views.post_user, name = 'post_user'),
+    # path('<int:pk>/update/', views.update_user, name = 'update_user'),
+    # path('<int:pk>/delete/', views.delete_user, name = 'delete_user'),
+    path('', PersonListView.as_view(), name='person-list'),  
+    path('<int:pk>/', PersonDetailView.as_view(), name='person-detail'),
 
 ]
--- myproject/api/views.py
+++ myproject/api/views.py
@@ -1,46 +1,81 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, APIView
 from .serializer import PersonSerializer
 from rest_framework import status
 
 from .models import Person
+#Function based view 
 
-@api_view(['GET'])
-def get_user(request):
-    person = Person.objects.all()
-    serializer = PersonSerializer(person, many=True)
-    return Response(serializer.data)
+# @api_view(['GET'])
+# def get_user(request):
+#     person = Person.objects.all()
+#     serializer = PersonSerializer(person, many=True)
+#     return Response(serializer.data)
 
-@api_view(['POST'])
-def post_user(request):
-    serializer = PersonSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['POST'])
+# def post_user(request):
+#     serializer = PersonSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['PUT'])
-def update_user(request, pk):
-    try:
-        person = Person.objects.get(pk=pk)
-    except Person.DoesNotExist:
-        return Response({'error': 'NOt found'}, status=status.HTTP_401_NOT_FOUND)
-    serializer = PersonSerializer(person, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
+# @api_view(['PUT'])
+# def update_user(request, pk):
+#     try:
+#         person = Person.objects.get(pk=pk)
+#     except Person.DoesNotExist:
+#         return Response({'error': 'NOt found'}, status=status.HTTP_401_NOT_FOUND)
+#     serializer = PersonSerializer(person, data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# @api_view(['DELETE'])
+# def delete_user(request, pk):
+#     try:
+#         person = Person.objects.get(pk=pk)
+#     except Person.DoesNotExist:
+#         return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+
+#     person.delete()
+#     return Response({'message': 'User deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+#Class Based View
+class PersonListView(APIView):
+    def get(self, request):
+        persons = Person.objects.all()
+        serializer = PersonSerializer(persons, many=True)
         return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def post(self, request):
+        serializer = PersonSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class PersonDetailView(APIView):
+    def put(self, request, pk):
+        try:
+            person = Person.objects.get(pk=pk)
+        except Person.DoesNotExist:
+            return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
 
-@api_view(['DELETE'])
-def delete_user(request, pk):
-    try:
-        person = Person.objects.get(pk=pk)
-    except Person.DoesNotExist:
-        return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
-
-    person.delete()
-    return Response({'message': 'User deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
-
+        serializer = PersonSerializer(person,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        try:
+            person = Person.objects.get(pk=pk)
+        except Person.DoesNotExist:
+            return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+        person.delete()
+        return Response({'message': 'User deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
